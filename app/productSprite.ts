74be27e70eb6e9a9154898bebe794a3d6c6834bc
Jonatan Maria <117693967+infoboy27@users.noreply.github.com>
2026-09-12 21:49:13 -0400

    assets: assemble MO Vida product sprite

---
+++ app/productSprite.ts
@@ -0,0 +1,6 @@
+import { productSpriteChunk0 } from "./productSprite/chunk0";
+import { productSpriteChunk1 } from "./productSprite/chunk1";
+import { productSpriteChunk2 } from "./productSprite/chunk2";
+import { productSpriteChunk3 } from "./productSprite/chunk3";
+
+export const PRODUCT_SPRITE = `data:image/webp;base64,${productSpriteChunk0}${productSpriteChunk1}${productSpriteChunk2}${productSpriteChunk3}`;
